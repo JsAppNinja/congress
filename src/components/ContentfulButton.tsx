c145a92fdfa141aa8d0c6bd38f4315e1e2e323f1
Connor Barrett <connor.barrett.dev@gmail.com>
2019-07-29 13:52:47 -0700

    QA Mobile contentful button (#108)

--- src/components/ContentfulButton.tsx
+++ src/components/ContentfulButton.tsx
@@ -16,17 +16,19 @@ const ContentfulButton: React.FC<Props> = ({
   return (
     <div className="col-12 pl1 md:pl0">
       <p className="franklin-gothic text-md">{description}</p>
-      <Button
-        className="ContentfulButton pointer mt1 col-6 itc-franklin-gothic-demi-compressed text-md"
-        ariaLabel={description}
-        isAnchor={true}
-        openInNewTab={true}
-        to={url}
-      >
-        <span className="flex flex-col justify-center items-center h100 col-12 pt_5">
-          {buttonText}
-        </span>
-      </Button>
+      <div className="col-6 mt1">
+        <Button
+          className="ContentfulButton pointer col-12 itc-franklin-gothic-demi-compressed text-md"
+          ariaLabel={description}
+          isAnchor={true}
+          openInNewTab={true}
+          to={url}
+        >
+          <span className="flex flex-col justify-center items-center h100 pt_5">
+            {buttonText}
+          </span>
+        </Button>
+      </div>
     </div>
   );
 };
